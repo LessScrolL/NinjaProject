78fe8532ca803768c9601967724f5eeaa65de9a7
Cynthia Castro <123767765+LessScrolL@users.noreply.github.com>
2024-01-05 13:42:02 -0600

    Update sandbox.js
    Create a sandbox.js

---
+++ Chapter_3/sandbox.js
@@ -0,0 +1,12 @@
+// for loops
+
+// for(let i = 0; i < 5; i++){
+//     console.log('in loop:', i);
+// }
+// console.log('loop finished');
+
+const names = ['shawn', 'Robert', 'Olga'];
+
+for(let i = 0; i < names.length; i++){
+    
+}
